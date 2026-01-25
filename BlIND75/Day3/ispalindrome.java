3313cc45b169d0ce4a92d9723d488b9b027586e3
Manish M Rao <manishmahesh456@gmail.com>
2026-01-25 13:18:13 +0530

    hello

---
+++ BlIND75/Day3/ispalindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            char l = s.charAt(left);
+            char r = s.charAt(right);
+            if (!Character.isLetterOrDigit(l)) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(r)) {
+                right--;
+                continue;
+            }
+            l = Character.toLowerCase(l);
+            r = Character.toLowerCase(r);
+            if (l != r) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
